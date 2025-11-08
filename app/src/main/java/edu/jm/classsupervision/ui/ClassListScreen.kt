bb5827e4681c5d7a9996f43765cec2aec862cd80
jandermoreira <moreira.jander@gmail.com>
2025-11-08 10:34:33 -0300

    Clean unused code

--- app/src/main/java/edu/jm/classsupervision/ui/ClassListScreen.kt
+++ app/src/main/java/edu/jm/classsupervision/ui/ClassListScreen.kt
@@ -3,7 +3,6 @@ package edu.jm.classsupervision.ui
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
