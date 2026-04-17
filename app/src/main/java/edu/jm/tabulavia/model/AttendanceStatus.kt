f62d286eb9a153defb2e9d456e138c2df094647e
jandermoreira <moreira.jander@gmail.com>
2026-04-17 08:15:43 -0300

    Remove comment

--- app/src/main/java/edu/jm/tabulavia/model/AttendanceStatus.kt
+++ app/src/main/java/edu/jm/tabulavia/model/AttendanceStatus.kt
@@ -2,7 +2,7 @@ package edu.jm.tabulavia.model
 
 import kotlinx.serialization.Serializable
 
-@Serializable // Anotação para serialização
+@Serializable
 enum class AttendanceStatus(val displayName: String) {
     PRESENT("Presente"),
     ABSENT("Ausente"),
