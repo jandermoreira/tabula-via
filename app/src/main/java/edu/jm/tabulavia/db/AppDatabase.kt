13813447f646c6260e2f971211948267c96788a8
jandermoreira <moreira.jander@gmail.com>
2026-07-30 19:08:57 -0300

    Increment database version to 19

--- app/src/main/java/edu/jm/tabulavia/db/AppDatabase.kt
+++ app/src/main/java/edu/jm/tabulavia/db/AppDatabase.kt
@@ -44,7 +44,7 @@ import edu.jm.tabulavia.model.StudentStatus
         SkillAssessment::class,
         ActivityHighlightedSkill::class
     ],
-    version = 18,
+    version = 19,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
